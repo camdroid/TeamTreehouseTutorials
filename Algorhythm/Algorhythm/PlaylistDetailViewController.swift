cae399ec6dc3cfb485f17ac66d181c3d94db9f8a
Cam <camherringshaw@gmail.com>
2015-01-20 22:21:49 -0500

    Initial commit

---
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorhythm
+//
+//  Created by Cam on 1/20/15.
+//  Copyright (c) 2015 camdroid. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Algorhythm/Algorhythm/PlaylistMasterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Algorhythm
+//
+//  Created by Cam on 1/20/15.
+//  Copyright (c) 2015 camdroid. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+                            
+    @IBOutlet weak var aButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        aButton.setTitle("Press me!", forState: .Normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
