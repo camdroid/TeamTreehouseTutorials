659ee798ccc7520c68c39f5f3e0bedb5667a4b15
Cam <camherringshaw@gmail.com>
2015-01-23 17:29:47 -0500

    Finished Achievement 2 of tutorial
    “Build a Playlist Browser with Swift”

--- Algorhythm/Algorhythm/PlaylistDetailViewController.swift
+++ Algorhythm/Algorhythm/PlaylistDetailViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class PlaylistDetailViewController: UIViewController {
 
     @IBOutlet weak var buttonPressLabel: UILabel!
+    var segueLabelText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        buttonPressLabel.text = segueLabelText
         // Do any additional setup after loading the view.
     }
 
